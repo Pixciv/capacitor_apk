3d12a3639e4ed46d271d50aaa454c3fd8a0784be
Pixciv <snackzmind@gmail.com>
2025-09-16 14:45:15 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -3,6 +3,7 @@ package com.arvinapp.pdfreader;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowInsets;
 import android.graphics.Color;
 import androidx.annotation.Nullable;
 import com.getcapacitor.BridgeActivity;
@@ -20,15 +21,19 @@ public class MainActivity extends BridgeActivity {
         getWindow().setStatusBarColor(Color.BLACK);
         getWindow().setNavigationBarColor(Color.BLACK);
 
-        // Status bar ve nav bar ikonlarını beyaz yap (tek seferde flag)
+        // Status bar ve nav bar ikonlarını beyaz yap (tüm flagleri birleştirerek)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             int flags = getWindow().getDecorView().getSystemUiVisibility();
-
-            // LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR flag'lerini kaldır → ikonlar beyaz
+            
+            // LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR flag'lerini kaldır
             flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
             }
+            
+            // FULLSCREEN veya diğer immersive flag’leri kaldır (bazı cihazlarda override edebiliyor)
+            flags &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            flags &= ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
             getWindow().getDecorView().setSystemUiVisibility(flags);
         }
