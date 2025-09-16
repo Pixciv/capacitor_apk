41f0193d64e69a58289220126c86c331604c76d5
Pixciv <snackzmind@gmail.com>
2025-09-16 21:42:29 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,51 +1,22 @@
 package com.arvinapp.pdfreader;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import androidx.core.view.WindowCompat;
+import android.view.WindowManager;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-
-        // Sistemin pencereyi tam ekran yapmasını engelle.
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-
-        // Status bar ve navigation bar rengini tam siyah yap
-        getWindow().setStatusBarColor(Color.BLACK);
-        getWindow().setNavigationBarColor(Color.BLACK);
-
-        // Status bar ve navigation bar ikonlarını beyaz yap
-        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView())
-                .setAppearanceLightStatusBars(false);
-        WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView())
-                .setAppearanceLightNavigationBars(false);
         
-        // Uygulama içeriğinin sistem barlarıyla çakışmasını engelle
-        final View rootView = findViewById(android.R.id.content);
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.black));
+        getWindow().setNavigationBarColor(getResources().getColor(android.R.color.black));
         
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            rootView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-                @Override
-                public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
-                    int statusBarHeight = insets.getSystemWindowInsets().top;
-                    int navigationBarHeight = insets.getSystemWindowInsets().bottom;
-
-                    view.setPadding(
-                        view.getPaddingLeft(),
-                        statusBarHeight,
-                        view.getPaddingRight(),
-                        navigationBarHeight
-                    );
-
-                    return insets;
-                }
-            });
-        }
+        // Bu, status bar ikonlarını beyaz yapar
+        getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | 
+                                                     WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
     }
 }
