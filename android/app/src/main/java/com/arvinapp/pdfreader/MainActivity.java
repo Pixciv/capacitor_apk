102b2c029d94e4d0edeb6bb28e72f65f7b8a49fc
Pixciv <snackzmind@gmail.com>
2025-09-16 17:59:17 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,18 +1,16 @@
 package com.arvinapp.pdfreader;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
 import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Bu satır, web içeriğinizin sistem barlarının (status ve navigation) 
-        // altına taşmasını engeller ve içeriği barlara göre ayırır.
+
+        // Bu ayar, web içeriğinin sistem barlarının altına taşmasını engeller.
         WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
     }
 }
