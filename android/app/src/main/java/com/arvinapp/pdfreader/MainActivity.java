1215a8894d7fb90ebe7a9bc6a1befde2fdb37a94
Pixciv <snackzmind@gmail.com>
2025-09-16 13:19:22 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,5 +1,17 @@
 package com.arvinapp.pdfreader;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Status bar alanını ayrı bırak, web içerik altından başlasın
+    getWindow().setDecorFitsSystemWindows(true);
+
+    // İsteğe bağlı: status bar rengini beyaz yap (değiştirebilirsin)
+    getWindow().setStatusBarColor(android.graphics.Color.WHITE);
+  }
+}
