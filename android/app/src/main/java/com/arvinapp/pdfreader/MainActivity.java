70f0266f073508f1a8c8f36f872f3ef0f74cb11e
Pixciv <snackzmind@gmail.com>
2025-09-16 22:01:51 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.arvinapp.pdfreader;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.View;
+import android.view.WindowInsetsController;
 import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
@@ -11,12 +13,24 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-        
+
+        // Status bar ve navigation bar siyah
         getWindow().setStatusBarColor(getResources().getColor(android.R.color.black));
         getWindow().setNavigationBarColor(getResources().getColor(android.R.color.black));
-        
-        // Bu, status bar ikonlarını beyaz yapar
-        getWindow().getDecorView().setSystemUiVisibility(getWindow().getDecorView().getSystemUiVisibility() | 
-                                                     WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // İkonları beyaz yapmak için modern API
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+            }
+        } else {
+            // Eski sürümlerde
+            getWindow().getDecorView().setSystemUiVisibility(
+                    getWindow().getDecorView().getSystemUiVisibility() &
+                    ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR &
+                    ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            );
+        }
     }
 }
