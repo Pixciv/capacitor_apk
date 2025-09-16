b9f90fb4493d1af2569585bf5457811e65e3d94c
Pixciv <snackzmind@gmail.com>
2025-09-16 22:27:48 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -17,31 +17,38 @@ public class MainActivity extends BridgeActivity {
 
         Window window = getWindow();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) { // Android 15+ için
-            window.getDecorView().setOnApplyWindowInsetsListener((view, insets) -> {
-                // Status bar yüksekliğini al
-                int statusBarHeight = insets.getInsets(WindowInsets.Type.statusBars()).top;
-                
-                // View arka planını siyah yap
-                view.setBackgroundColor(Color.BLACK);
+        // Status bar ve navigation bar siyah, ikonlar beyaz
+        applyEdgeToEdgeBlackBars(window);
+    }
 
-                // İçeriğin status bar ile çakışmaması için padding ekle
-                view.setPadding(0, statusBarHeight, 0, 0);
+    private void applyEdgeToEdgeBlackBars(Window window) {
+        final int blackColor = Color.BLACK;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) { // Android 15+
+            window.getDecorView().setOnApplyWindowInsetsListener((view, insets) -> {
+                int statusBarHeight = insets.getInsets(WindowInsets.Type.statusBars()).top;
+                int navBarHeight = insets.getInsets(WindowInsets.Type.navigationBars()).bottom;
+
+                // Arka planı siyah ve padding ayarla
+                view.setBackgroundColor(blackColor);
+                view.setPadding(0, statusBarHeight, 0, navBarHeight);
 
                 return insets;
             });
 
-            // Status bar ikonlarını beyaz yap
-            window.getDecorView().setSystemUiVisibility(0);
-        } else {
-            // Android 15 altı cihazlar
-            window.setStatusBarColor(Color.BLACK);
-            // Beyaz ikonlar için legacy flag
-            window.getDecorView().setSystemUiVisibility(0);
-        }
+            // Status bar ve navigation bar ikonları beyaz
+            int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            window.getDecorView().setSystemUiVisibility(flags);
 
-        // Navigation bar siyah ve ikonlar beyaz
-        window.setNavigationBarColor(Color.BLACK);
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+            window.setStatusBarColor(blackColor);
+            window.setNavigationBarColor(blackColor);
+        } else {
+            // Android 15 ve altı
+            window.setStatusBarColor(blackColor);
+            window.setNavigationBarColor(blackColor);
+            window.getDecorView().setSystemUiVisibility(0); // ikonlar beyaz
+        }
     }
 }
