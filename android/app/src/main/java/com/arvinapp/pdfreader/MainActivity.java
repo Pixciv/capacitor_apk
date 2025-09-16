9072556b7048e536e81e868bda29a901f60cd264
Pixciv <snackzmind@gmail.com>
2025-09-16 23:34:42 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,12 +1,37 @@
 package com.arvinapp.pdfreader;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.graphics.Color;
+import androidx.annotation.Nullable;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // İçeriğin ekranın kenarlarına kadar uzamasını sağlar
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        // Status bar ve navigasyon barı renklerini ayarla
+        WindowInsetsControllerCompat windowInsetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+
+        // Status bar'ı siyah yapar
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.black));
+        // Status bar ikonlarını beyaz yapar (açık stil kapalı)
+        windowInsetsController.setAppearanceLightStatusBars(false);
+
+        // Navigasyon barı siyah yapar
+        getWindow().setNavigationBarColor(getResources().getColor(android.R.color.black));
+        // Navigasyon barı ikonlarını beyaz yapar (açık stil kapalı)
+        windowInsetsController.setAppearanceLightNavigationBars(false);
     }
 }
