5fa098f5e0f1132600fc5a6c0dc8307481341c8d
Pixciv <snackzmind@gmail.com>
2025-09-16 16:55:05 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -3,7 +3,6 @@ package com.arvinapp.pdfreader;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowInsets;
 import android.graphics.Color;
 import androidx.annotation.Nullable;
 import com.getcapacitor.BridgeActivity;
@@ -14,26 +13,27 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Capacitor WebView için tam ekran
-        getWindow().setDecorFitsSystemWindows(false);
-
         // Status bar ve navigation bar siyah yap
         getWindow().setStatusBarColor(Color.BLACK);
         getWindow().setNavigationBarColor(Color.BLACK);
 
-        // Status bar ve nav bar ikonlarını beyaz yap (tüm flagleri birleştirerek)
+        // WebView’in status/navigation bar’ın altında başlamasını engelle
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(true);
+        } else {
+            // Eski sürümlerde layout çakışmalarını önlemek için
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+
+        // Status bar ve nav bar ikonlarını beyaz yap
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             int flags = getWindow().getDecorView().getSystemUiVisibility();
-            
-            // LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR flag'lerini kaldır
+
+            // LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR flag’lerini kaldır
             flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
             }
-            
-            // FULLSCREEN veya diğer immersive flag’leri kaldır (bazı cihazlarda override edebiliyor)
-            flags &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-            flags &= ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
             getWindow().getDecorView().setSystemUiVisibility(flags);
         }
