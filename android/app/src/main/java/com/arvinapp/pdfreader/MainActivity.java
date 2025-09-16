1e7b08705a745c69a3e8385aec8a4c915dd8da00
Pixciv <snackzmind@gmail.com>
2025-09-16 17:03:44 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
+++ android/app/src/main/java/com/arvinapp/pdfreader/MainActivity.java
@@ -1,8 +1,6 @@
 package com.arvinapp.pdfreader;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.graphics.Color;
 import androidx.annotation.Nullable;
 import com.getcapacitor.BridgeActivity;
@@ -17,25 +15,12 @@ public class MainActivity extends BridgeActivity {
         getWindow().setStatusBarColor(Color.BLACK);
         getWindow().setNavigationBarColor(Color.BLACK);
 
-        // WebView’in status/navigation bar’ın altında başlamasını engelle
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(true);
-        } else {
-            // Eski sürümlerde layout çakışmalarını önlemek için
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
+        // İkonları beyaz yap
+        // (LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR kaldırıldı, böylece ikonlar beyaz kalır)
+        int flags = getWindow().getDecorView().getSystemUiVisibility();
+        getWindow().getDecorView().setSystemUiVisibility(flags);
 
-        // Status bar ve nav bar ikonlarını beyaz yap
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int flags = getWindow().getDecorView().getSystemUiVisibility();
-
-            // LIGHT_STATUS_BAR ve LIGHT_NAVIGATION_BAR flag’lerini kaldır
-            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-            }
-
-            getWindow().getDecorView().setSystemUiVisibility(flags);
-        }
+        // WebView artık status/navigation bar'ın üstüne düşmez, normal layout gibi davranır
+        // Tam ekran veya setDecorFitsSystemWindows(false) yok
     }
 }
